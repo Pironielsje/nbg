0035e6da79546b6221033ed445a39ad196d074e5
GlowCat <81696606+Pironielsje@users.noreply.github.com>
2022-03-08 16:34:53 +0100

    fixing error

--- commands/ping.js
+++ commands/ping.js
@@ -4,7 +4,7 @@ module.exports.run = async(client, msg, args) => {
 
     const embed = new MessageEmbed()
         .setTitle("Pong!")
-        .setDescription(`Api response time: ${Date.now() - msg.createdTimestamp()}ms.\nBot Latency: ${Math.floor(Math.round(client.ws.ping))}ms`)
+        .setDescription(`Api response time: ${Date.now() - msg.createdTimestamp}ms.\nBot Latency: ${Math.floor(Math.round(client.ws.ping))}ms`)
         .setColor("RANDOM")
         .setFooter(`Requested by: ${msg.author.username}`, msg.author.displayAvatarUrl())
         .setTimestamp()
