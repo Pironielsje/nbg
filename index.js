3cb670cb6e4123d2b03d6b176d12be02d9033441
AlgoGames <81696606+Pironielsje@users.noreply.github.com>
2022-03-09 11:26:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ for (const file of cmdFiles) {
 
 }
 
-client.once(`ready`, () => {
+client.on(`ready`, () => {
     console.log(`Ja ben er!`)
     client.user.setActivity(`There are ${client.guilds.cache.get('950761668898594846').memberCount} kids in this basement!`)
 
