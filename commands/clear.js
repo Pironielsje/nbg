dfa40d15f12600e4d04e0fc405f0f4912d2dda83
AlgoGames <81696606+Pironielsje@users.noreply.github.com>
2022-03-09 12:15:40 +0100

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -1,28 +1,36 @@
 module.exports.run = async(client, msg, args) => {
-    if (!msg.member.permissions.has("MANAGE_MESSAGES")) return msg.reply(`You don't have the **MANAGE_MESSAGES** permission.`)
+        if (!msg.member.permissions.has("MANAGE_MESSAGES")) return msg.reply(`You don't have the **MANAGE_MESSAGES** permission.`)
 
-    if (!args[0]) return msg.reply(`Please specify how much messages to clear`)
+        if (!args[0]) return msg.reply(`Please specify how much messages to clear`)
 
-    if (parseInt(args[0])) {
+        if (parseInt(args[0])) {
 
-        let amount = args[0] + 1;
+            let amount = args[0] + 1;
 
-        msg.channel.bulkDelete(amount).then(() => {
+            msg.channel.bulkDelete(amount).then(() => {
 
-            msg.channel.send(`Deleted ${amount} message(s)!`).then(m => {
-                setTimeout(() => {
-                    m.delete()
-                }, 2000);
+                msg.channel.send(`Deleted ${amount} message(s)!`).then(m => {
+                    setTimeout(() => {
+                        m.delete()
+                    }, 2000);
+                })
+
             })
 
-        })
+        } else if (parseInt(args[0] >= 100)) {
 
-    } else {
-        return msg.reply(`That isn't a valid number!`)
-    }
-}
+            msg.reply(`99 is the maximum number of messages you can delete.`)
 
-module.exports.help = {
-    name: "clear",
-    aliases: ["purge"]
-}
+        } else if (parseInt(args[0]) <= 0 {
+
+                msg.reply(`You can't delete less than 1 message!`)
+
+            } else {
+                return msg.reply(`That isn't a valid number!`)
+            }
+        }
+
+        module.exports.help = {
+            name: "clear",
+            aliases: ["purge"]
+        }
